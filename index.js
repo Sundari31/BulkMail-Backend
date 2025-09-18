d37524ed8db3a688a0d1c54e4b710cb2cbb489c1
Sundari31 <sundarimuthaiah2004@gmail.com>
2025-09-18 20:36:29 +0530

    Initial commit - bulkmail backend

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express")
+const cors = require("cors")
+const nodemailer = require("nodemailer")
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(function () {
+        console.log("Connected to DB")
+    }).catch(function () {
+        console.log("Failed to Connect")
+    })
+
+const credential = mongoose.model("credential", {}, "bulkmail")
+
+app.post("/sendemail", function (req, res) {
+    var msg = req.body.msg
+    var emailList = req.body.emailList
+
+    credential.find().then(function (data) {
+
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS,
+            }
+        })
+
+        new Promise(async function (resolve, reject) {
+            try {
+                for (var i = 0; i < emailList.length; i++) {
+                    await transporter.sendMail(
+                        {
+                            from: "sundarimuthaiah2004@gmail.com",
+                            to: emailList[i],
+                            subject: "A message from BulkMail App",
+                            text: msg
+                        }
+                    )
+                    console.log("Email sent to:" + emailList[i])
+                }
+                resolve("Success")
+            }
+            catch (error) {
+                reject("Failed")
+            }
+        }).then(function () {
+            res.send(true)
+        }).catch(function () {
+            res.send(false)
+        })
+
+    }).catch(function (error) {
+        console.log(error)
+    })
+
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, function () {
+    console.log(`Server running on port ${PORT}`);
+})
