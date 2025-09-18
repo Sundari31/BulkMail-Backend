2921d6689011c77b8c61def355906d9976a63c5b
Sundari31 <sundarimuthaiah2004@gmail.com>
2025-09-18 20:59:23 +0530

    Updated the Backend

--- index.js
+++ index.js
@@ -62,7 +62,5 @@ app.post("/sendemail", function (req, res) {
 
 })
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, function () {
-    console.log(`Server running on port ${PORT}`);
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
